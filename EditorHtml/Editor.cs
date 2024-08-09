229b391d21d4bbcf6230d214ad582497f902325f
Carlos Daniel <carlos.oficial.ti@gmail.com>
2024-08-09 12:19:01 -0300

    Função para substituir textos em negrito apenas

--- EditorHtml/Editor.cs
+++ EditorHtml/Editor.cs
@@ -24,6 +24,7 @@ namespace EditorHtml {
 
             Console.WriteLine("-----------");
             Console.WriteLine(" Deseja salvar o arquivo ?");
+            Viewer.Show(file.ToString());
 
 
 
